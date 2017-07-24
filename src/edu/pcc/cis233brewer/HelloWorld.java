64f56285907f8d4c3f9ff3d8dacc33030072b93b
DanielBrewerPcc <daniel.brewer1@pcc.edu>
2017-07-24 12:50:56 -0700

    added main method that prints a few messages

--- src/edu/pcc/cis233brewer/HelloWorld.java
+++ src/edu/pcc/cis233brewer/HelloWorld.java
@@ -1,4 +1,19 @@
 package edu.pcc.cis233brewer;
 
-public class HelloWorld {
+/**
+ * @class HelloWorld is the main class for the HelloWorld project.
+ * It will contain main functions and other project-level coce
+ * @author Daniel Brewer
+ * @version 2017-07-20-001
+ */
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is the HelloWorld project by Daniel Brewer. ");
+        System.out.println("Hello World is a project for Walter Lara's CIS 233J class.");
+
+    }
 }
+
